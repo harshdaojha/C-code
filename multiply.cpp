f7d5a1d32590a64edc1f01fe4ff490332e186552
Harshda <90568951+harshdaojha@users.noreply.github.com>
2022-10-14 16:48:51 +0530

    multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1 , num2;
+  cout<<"First number: "<<num1;
+  cout<<"Second number: "<<num2;
+  
+  cout<<"Result: "<<num1*num2;
+  
+  return 0;
+}
