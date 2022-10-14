739a17d7ab5efe22da3510b06b04b3fb04a2459e
Harshda <90568951+harshdaojha@users.noreply.github.com>
2022-10-14 16:43:56 +0530

    addNumber.cpp

---
+++ addNumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1, num2;
+  cout<<"First Number: "<<num1;
+  cout<<"Second Number: "<<num2;
+  
+  cout<<"Result: "<<num1+num2;
+  
+  return 0;
+}
